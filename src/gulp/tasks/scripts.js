ee6405878384337e963fdbb34d1933c0c25f7302
Dennis Kortekaas <dennis@axendo.nl>
2018-11-12 16:59:35 +0100

    Fixed Gulp script task

--- src/gulp/tasks/scripts.js
+++ src/gulp/tasks/scripts.js
@@ -3,10 +3,10 @@
 module.exports = function (gulp, plugins, output) {
     return function () {
 
-        var shouldMinify = process.env.NODE_ENV === "production";
+        var shouldMinify = true; //process.env.NODE_ENV === "production";
 
         // Custm scripts
-        gulp.src('src/scripts/**/*.js')
+        gulp.src('src/js/scripts.js')
             //.pipe(changed(output))
             .pipe(plugins.concat('scripts.min.js'))
             .pipe(gulpif(shouldMinify, plugins.uglify()))
