1005a453a72f3d54deb4f53bd50f241e36b1e2b1
Dennis Kortekaas <dennis@symsys.nl>
2020-02-11 13:49:14 +0100

    WC update + styling fix

--- woocommerce/single-product/related.php
+++ woocommerce/single-product/related.php
@@ -10,10 +10,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see           https://docs.woocommerce.com/document/template-structure/
- * @author        WooThemes
- * @package       WooCommerce/Templates
- * @version       3.0.0
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce/Templates
+ * @version     3.9.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -37,9 +36,10 @@ if ( $related_products ) : ?>
 					<?php
 					$post_object = get_post( $related_product->get_id() );
 
-					setup_postdata( $GLOBALS['post'] =& $post_object );
+					setup_postdata( $GLOBALS['post'] =& $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
-					wc_get_template_part( 'content', 'product' ); ?>
+					wc_get_template_part( 'content', 'product' );
+					?>
 
 				<?php endforeach; ?>
 
@@ -47,7 +47,7 @@ if ( $related_products ) : ?>
 			</div>
 		</div>
 	</section>
-
-<?php endif;
+	<?php
+endif;
 
 wp_reset_postdata();
